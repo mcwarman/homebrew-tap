bf1a0e350caf5e0903d1a6f96f1a75b30137d4c0
Matthew Warman <matthew.warman@proagrica.com>
2022-01-19 15:30:29 +0000

    jolt: add 0.1.5 bottle.

--- Formula/jolt.rb
+++ Formula/jolt.rb
@@ -4,6 +4,13 @@ class Jolt < Formula
   url "https://github.com/mcwarman/jolt-cli/releases/download/v0.1.5/jolt-cli.tar.gz"
   sha256 "d55d34f76c35bec77c6ba8878656f9b1cfbd75193e1d28d1922f8ea12c32828f"
 
+  bottle do
+    root_url "https://github.com/mcwarman/homebrew-tap/releases/download/jolt-0.1.5"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, big_sur:      "f3d536b429b37ea1e9089d1a884e1a0e046f201f05b92862f31ad22dae2bf405"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "7b85bc7f95799f70176fd22f39228cb960a32cc95c42c2bc96ccea9abf987712"
+  end
+
   depends_on "openjdk"
 
   def install
